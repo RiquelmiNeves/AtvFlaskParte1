1c2181c4239b38e19ba5bf199c864ae99b8f1887
Riquelmi neves <riquelmeneves998@gmail.com>
2025-10-22 15:10:28 +0000

    Feat: Telas com componetes vindos do Base.html

--- aula01.py
+++ aula01.py
@@ -1,54 +1,33 @@
-#Criamos no decorador app_Mariela com página dinâmica
 from flask import Flask, render_template
 
-app_rick = Flask(__name__)
- 
+app_rick = Flask(__name__, template_folder='templates') 
+# os templates coloca em outra pasta. 
+# Por padrão, fica na pasta templates e não precisa informar no template_folder,
+# mas se quiser armazenar em outra pasta indique nesse parâmetro.
 
-@app_rick.route("/")  #rota para solicitação web
-def homepage():     #função da rota
-    return render_template ("homepage.html")
-
-@app_rick.route("/index")
+@app_rick.route("/")       #se no navegador digitar / ou /index
+@app_rick.route("/index")  
 def indice():
-    return render_template ("index.html") 
+    return render_template ("t_index.html") #optei por prefixar com t_ os nomes dos arquivos que usam template
 
 @app_rick.route("/contato")
 def contato():
-    return render_template("contato.html") 
+    return render_template("t_contato.html") 
 
-@app_rick.route("/usuario")
-def dados_usuario():
-    #nome_usuario="Riquelmi"
-    dados_usu = {"nome": "Riquelmi", "profissao": "Estagiario", "disciplina":"Desenvolvimento Web III"}
-    return render_template("usuario.html", dados = dados_usu)
-                                           #parâmetro recebe argumento
-                                           #colocar o site no ar
-"""
-  Página dinâmica com argumentos via URL. Precisa de 3 elementos: 
-  o dado dinâmico na URL entre < >, 
-  os parâmetros na função e 
-  a página montada dinamicamente.
-"""
-@app_rick.route('/usuario/<id>')
-def saudacao(id):
-    #codigo para conectar com BD
-    #select nome, profissao, disciplina 
-    # from usario where usuario=id
-    # resultados 
-    return render_template('homepage_nome.html', nome=id)
+@app_rick.route("/login")
+def login():
+    return render_template("t_login.html") 
 
-@app_rick.route("/usuario/<nome_usuario>;<nome_profissao>;<nome_disciplina>") 
-#o que está entre < > é o dado dinâmico que vai diferenciar a página para cada usuário.
-def usuario (nome_usuario, nome_profissao, nome_disciplina): #passa o valor da variável como argumento na função
-    
-    dados_usu = {"profissao": nome_profissao, "disciplina": nome_disciplina}
-    #dados_usu é um dicionário com 2 chaves:valor
-
-    return render_template ("usuarioURL.html", nome=nome_usuario, dados = dados_usu)  
-        #o parâmetro 'nome' recebe como argumento o valor armazenado na variável 'nome_usuario'
-        #o parâmetro 'dados' recebe como argumento o dicionário dados_usu
+@app_rick.route("/usuario", defaults={"nome_usuario":"usuário?","nome_profissao":""}) 
+def usuarios (nome_usuario, nome_profissao):
+    dados_usu = {"profissao": nome_profissao, "disciplina":"Desenvolvimento Web III"}
+    return render_template ("t_usuario.html", nome=nome_usuario, dados = dados_usu)  
 
 
-#colocar o site no ar
+#rota /usuarios COM passagem de argumentos
+#@app_rick.route("/usuario/<nome_usuario>;<nome_profissao>")
+#rota /usuarios SEM passagem de argumentos --> definir valor padrão com defaults
+
 if __name__ == "__main__": 
      app_rick.run(port = 8000) 
+     
