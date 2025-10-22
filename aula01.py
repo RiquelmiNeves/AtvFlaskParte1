436debd5dfd0928b607b3fda2c840f1f999a8558
Riquelmi neves <riquelmeneves998@gmail.com>
2025-10-22 13:16:20 +0000

    Feat: Primeira rota aula 2

--- aula01.py
+++ aula01.py
@@ -1,9 +1,19 @@
-from flask import Flask
 
-app_rick = Flask (__name__)
+from flask import Flask  #importa a biblioteca. flask diferente de Flask
+
+app_rick = Flask (__name__) #cria objeto da aplicação. 
+                               # Tem 2 underlines antes e depois de name
 
 @app_rick.route('/')
-def raiz():
+@app_rick.route('/ola') # rota para solicitação web
+                           # @ significa decorador
+def raiz():                # função a ser executa quando chamar rota acima ('/ola')
     return 'Olá, turma!'
 
-app_rick.run()
+def saudacoes (nome):
+    return f'Olá, {nome}!'
+
+if __name__ == "__main__" : # Dois underlines no name e no main
+    # diferenciar quando é executado aqui, como um script princial
+    # ou é invocado por fora, quando importado por um módulo
+    app_rick.run(port = 8000)  #executa aplicação
