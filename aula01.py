f4751f5745fdbf1ec3a585166a21e77270065fff
Riquelmi neves <riquelmeneves998@gmail.com>
2025-10-22 13:44:04 +0000

    Feat: Paginas com templates e nova rota

--- aula01.py
+++ aula01.py
@@ -1,20 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app_rick = Flask (__name__) #cria objeto da aplicação. 
+app_rick = Flask(__name__ , template_folder='templates')
+#cria o objeto da aplicação
 
-@app_rick.route('/')    #rota para solicitação web
-@app_rick.route('/rota1') #rota para solicitação web
-def rota1():  # função a ser executa quando chamar rotas acima ('/') e ('/rota1')
-    return 'Olá, turma!'
+@app_rick.route("/")  #rota para solicitação web
+def homepage():          #função
+    return render_template ("homepage.html")
 
-@app_rick.route('/rota2')
-def rota2():
-    resposta = "<H3> Essa é outra página da rota 2 <H3>"
-    return resposta
+@app_rick.route("/contato")
+def contato():
+    return render_template("contato.html") 
 
-#função que não pertence a nenhuma rota. 
-def saudacoes (nome): 
-    return f'Olá, {nome}'
-
-if __name__ == "__main__" :
-    app_rick.run(port = 8000)  #executa aplicação
+if __name__ == "__main__": 
+     app_rick.run(port = 8000) 
